b02f8d5f0fc8ae515a1422eacd8b50a2db056195
erfanscode <codeerfan@gmail.com>
2024-10-04 09:43:38 +0330

    create custom user model

--- social/models.py
+++ social/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    # Custom user model
+    date_of_birth = models.DateField(null=True, blank=True, verbose_name="تاریخ تولد")
+    bio = models.TextField(max_length=120, null=True, blank=True, verbose_name="بایو")
+    photo = models.ImageField(upload_to="account_images/", null=True, blank=True, verbose_name="تصویر")
+    job = models.CharField(max_length=120, null=True, blank=True, verbose_name="شغل")
+    phone = models.CharField(max_length=11, verbose_name="موبایل")
