e95aa298bbce0614704d6eefad59b6342c7878d9
erfanscode <codeerfan@gmail.com>
2024-10-04 09:52:39 +0330

    add admin config for showing user information in django admin panel

--- social/admin.py
+++ social/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    # Show user information in admin panel
+    list_display = ['username', 'first_name', 'last_name', 'phone']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Information', {'fields': ('date_of_birth', 'bio', 'photo', 'job', 'phone')}),
+    )
