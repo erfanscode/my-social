378c3caff1f774e39a0a2cd88385efbfd37d0992
erfanscode <codeerfan@gmail.com>
2024-10-16 04:03:30 +0330

    add change password and reset password urls

--- social/urls.py
+++ social/urls.py
@@ -12,4 +12,13 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('user/edit', views.edit_user, name='edit_user'),
     path('ticket/', views.ticket, name='ticket'),
+    path("password-change/", auth_view.PasswordChangeView.as_view(success_url='done'), name="password_change"),
+    path("password-change/done", auth_view.PasswordChangeDoneView.as_view(), name="password_change_done"),
+    path("password-reset/", auth_view.PasswordResetView.as_view(success_url="done"), name="password_reset"),
+    path("password-reset/done", auth_view.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path("password-reset/<uidb64>/<token>/",
+         auth_view.PasswordResetConfirmView.as_view(success_url="/password-reset/complete"),
+         name="password_reset_confirm"
+    ),
+    path("password-reset/complete/", auth_view.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
