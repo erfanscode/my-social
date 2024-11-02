3c8cd6d48aff448a8f1956b6eef890967d4d28f5
erfanscode <codeerfan@gmail.com>
2024-11-02 04:25:30 +0330

    send email to post author when delete a post by admin

--- social/signals.py
+++ social/signals.py
@@ -1,5 +1,7 @@
-from django.db.models.signals import m2m_changed
+from django.db.models.signals import m2m_changed, post_delete
 from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.conf import settings
 from .models import Post
 
 
@@ -7,3 +9,10 @@ from .models import Post
 def users_like_change(sender, instance, **kwargs):
     instance.total_likes = instance.likes.count()
     instance.save()
+
+@receiver(post_delete, sender=Post)
+def post_delete_send_email(sender, instance, **kwargs):
+    author = instance.author
+    subject = "پست شما حذف شد"
+    message = f"پست شما با آیدی {instance.id} حذف شد."
+    send_mail(subject, message, settings.EMAIL_HOST_USER, [author.email], fail_silently=False)
